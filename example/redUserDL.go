d96be63dd57a021d9fd93386f7ea4b46f715c05c
eehsiao <eehsiao@gmail.com>
2019-09-04 09:05:05 +0800

    redis data logical with data object

--- example/redUserDL.go
+++ example/redUserDL.go
@@ -6,10 +6,12 @@ import (
 	"github.com/eehsiao/go-models/redis"
 )
 
+// RedUserModel : extend from redis.Dao
 type RedUserModel struct {
 	*redis.Dao
 }
 
+// User : json struct that to store into redis
 type User struct {
 	Host       string `json:"host"`
 	User       string `json:"user"`
@@ -17,7 +19,7 @@ type User struct {
 }
 
 // UserHMSet : this is a data logical function, you can write more logical in there
-// sample data logical function
+// sample function of the data logical
 func (r *RedUserModel) UserHMSet(hKey string, kv map[string]interface{}) (status string, err error) {
 	if kv != nil && len(kv) > 0 {
 		return r.HMSet(hKey, kv).Result()
